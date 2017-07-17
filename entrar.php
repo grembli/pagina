d85abb417268aaa713f5ef40bd445a70711ec2fc
Jose <grembliluf@gmail.com>
2017-07-17 11:40:27 +0200

    Pruebas en php correctas y añadidos nombres a los input en ingreso.html

--- entrar.php
+++ entrar.php
@@ -2,14 +2,33 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Resultados</title>
   </head>
   <body>
     <?php
-      $usuario = $_POST['usuario'];
-      $password = $_POST['password'];
+      $conexion = mysql_connect('127.0.0.1',"root","comunismo");
+      mysql_select_db("prueba",$conexion);
+      $usuario = $_POST['user'];
+      $password = $_POST['pass'];
+
+      if ($conexion != false) {
+
+        $consulta = mysql_query("select frase1,frase2,frase3 from frases",$conexion);
+
+      }
+      $var1 = mysql_result($consulta,0,"frase1");
+      $var2 = mysql_result($consulta,0,"frase2");
+      $var3 = mysql_result($consulta,0,"frase3");
+
+      $conjunto = $var1." ".$var2." ".$var3;
+      mysql_free_result($consulta);
+      mysql_close($conexion);
 
       echo "Eres: ".$usuario." y tu contraseña es: ".$password;
+
+      echo "<br/>";
+
+      echo "El resultado es: ".$conjunto;
     ?>
   </body>
 </html>
